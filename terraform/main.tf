4482d63d9f1d3fe472b37c21d71629185f2bf12f
“Donte7” <noturavgnerd@myyahoo.com>
2026-07-19 03:59:18 -0400

    style: run terraform fmt to fix formatting inconsistencies

--- terraform/main.tf
+++ terraform/main.tf
@@ -139,7 +139,7 @@ resource "azurerm_linux_virtual_machine" "splunk" {
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    disk_size_gb          = var.os_disk_size_gb
+    disk_size_gb         = var.os_disk_size_gb
   }
 
   source_image_reference {
